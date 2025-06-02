58dcbb1f0959bb5cb5566fb8c32e2d07d938841d
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-06-02 13:03:36 -0300

    Versão inicial do app

---
+++ app/src/main/java/com/aula/exame_periodico_desafio/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.aula.exame_periodico_desafio.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
